820eabaebd6ba49908fbb643dc4a82a17bdc9852
Lauren Middleton <git@laurenmiddleton.com>
2015-09-27 14:07:27 -0400

    updated works on paper thumb layout

--- laurenmiddleton.com/works-on-paper.php
+++ laurenmiddleton.com/works-on-paper.php
@@ -6,119 +6,94 @@
 </head>
 
 <body>
-<table width="700" align="center" cellpadding="4" bordercolor="#ECECEC" id="maincontent" vspace="50">
   
   <?php include 'header.php';?>
+
+  <div class="maincontent">
   
-  <tr>
-    <td colspan="3" align="center" valign="middle" bordercolor="#FFFFFF" class="caption">
-      <div align="center">
-        <table width="700" border="0" align="center" cellpadding="4">
-          <tr>
-            <td width="37" class="navigation" style="text-align: center">&nbsp;</td>
-            <td width="150" class="navigation" style="text-align: center">
-              <span class="navigation" style="text-align: center">
-                <span class="navigation" style="text-align: center">
-                  <a href="cc2a.html"><img src="works_on_paper/67cc2a_th.jpg" width="150" height="150"></a>
-                </span>
-              </span>
-            </td>
-            <td width="150" class="navigation" style="text-align: center">
-              <span class="navigation" style="text-align: center">
-                <span class="navigation" style="text-align: center">
-                  <a href="cc2b.html"><img src="works_on_paper/68cc2b_th.jpg" width="150" height="150"></a>
-                </span>
-              </span>
-            </td>
-            <td width="150" class="navigation" style="text-align: center">
-              <span class="navigation" style="text-align: center">
-                <span class="navigation" style="text-align: center">
-                  <a href="cc2c.html"><img src="works_on_paper/69cc2c_th.jpg" width="150" height="150">
-                  </a>
-                </span>
-              </span>
-            </td>
-            <td width="150" class="navigation" style="text-align: center">
-              <a href="cc2i.html"><img src="works_on_paper/75cc2i_th.jpg" width="150" height="150"></a>
-            </td>
-            <td width="37" class="navigation" style="text-align: center">&nbsp;</td>
-          </tr>
-          <tr>
-            <td width="37" class="navigation" style="text-align: center">&nbsp;</td>
-            <td width="37" class="navigation" style="text-align: center">&nbsp;</td>
-          </tr>
-          <tr>
-            <td width="37" class="navigation" style="text-align: center">&nbsp;</td>
-            <td width="150" class="navigation" style="text-align: center">
-              <span class="navigation" style="text-align: center">
-                <span class="navigation" style="text-align: center">
-                  <a href="detach.html"><img src="works_on_paper/119_detatch_pinata_is_torn_th.jpg" width="150" height="150"></a>
-                </span>
-              </span>
-            </td>
-            <td width="150" class="navigation" style="text-align: center">
-              <span class="navigation" style="text-align: center">
-                <span class="navigation" style="text-align: center">
-                  <a href="young_again.html"><img src="works_on_paper/128_young_again_whole_again_th.jpg" width="150" height="150"></a>
-                </span>
-              </span>
-            </td>
-            <td width="150" class="navigation" style="text-align: center">
-              <span class="navigation" style="text-align: center">
-                <span class="navigation" style="text-align: center">
-                  <a href="vag1.html"><img src="works_on_paper/129_vag_collage_1_th.jpg" width="150" height="150"></a>
-                </span>
-              </span>
-            </td>
-            <td width="150" class="navigation" style="text-align: center">
-              <a href="vag2.html"><img src="works_on_paper/130_vag_collage_2_th.jpg" width="150" height="150"></a>
-            </td>
-            <td width="37" class="navigation" style="text-align: center">&nbsp;</td>
-          </tr>
-          <tr>
-            <td class="navigation" style="text-align: center">&nbsp;</td>
-            <td class="navigation" style="text-align: center">
-              <a href="do_it_yourself.html"><img src="works_on_paper/140_do_it_yourself_th.jpg" width="150" height="150"></a>
-            </td>
-            <td class="navigation" style="text-align: center">
-              <span class="navigation" style="text-align: center">
-                <span class="navigation" style="text-align: center">
-                  <a href="heartstrings.html"><img src="works_on_paper/141_heartstrings_th.jpg" width="150" height="150"></a>
-                </span>
-              </span>
-            </td>
-            <td class="navigation" style="text-align: center">
-              <span class="navigation" style="text-align: center">
-                <span class="navigation" style="text-align: center">
-                  <a href="vag3.html"><img src="works_on_paper/142_vagina_collage_3_th.jpg" width="150" height="150"></a>
-                </span>
-              </span>
-            </td>
-            <td class="navigation" style="text-align: center">
-              <span class="navigation" style="text-align: center">
-                <span class="navigation" style="text-align: center">
-                  <span class="navigation" style="text-align: center">
-                    <a href="vag4.html"><img src="works_on_paper/143_vagina_collage_4_th.jpg" width="150" height="150"></a>
-                  </span>
-                </span>
-              </span>
-            </td>
-            <td class="navigation" style="text-align: center">&nbsp;</td>
-          </tr>
-        </table>
-      </div></td>
-    </tr>
+    <ul class="thumb-nav">
 
-    <?php include 'footer.php';?>
+      <li>
+        <a href="works_on_paper/cc2a.html">
+          <img src="works_on_paper/67cc2a_th.jpg" alt="Crop Circle Series #2A">
+        </a>
+      </li>
 
-</table>
-<br />
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "UA-3411571-1";
-urchinTracker();
-</script>
+      <li>
+        <a href="works_on_paper/cc2b.html">
+          <img src="works_on_paper/68cc2b_th.jpg" alt="Crop Circle Series #2B">
+        </a>
+      </li>
+
+      <li>
+        <a href="works_on_paper/cc2c.html">
+          <img src="works_on_paper/69cc2c_th.jpg" alt="Crop Circle Series #2C">
+        </a>
+      </li>
+      
+      <li>
+        <a href="works_on_paper/cc2i.html">
+          <img src="works_on_paper/75cc2i_th.jpg" alt="Crop Circle Series #2I">
+        </a>
+      </li>
+
+    </ul>
+
+    <ul class="thumb-nav">
+
+      <li>
+        <a href="works_on_paper/detach.html">
+          <img src="works_on_paper/119_detatch_pinata_is_torn_th.jpg" alt="Detach (Your Pinata is Torn)">
+        </a>
+      </li>
+      
+      <li>
+        <a href="works_on_paper/young_again.html">
+          <img src="works_on_paper/128_young_again_whole_again_th.jpg" alt="young again, whole again">
+        </a>
+      </li>
+      
+      <li>
+        <a href="works_on_paper/vag1.html">
+          <img src="works_on_paper/129_vag_collage_1_th.jpg" alt="Vagina Collage 1">
+        </a>
+      </li>
+      
+      <li>
+        <a href="works_on_paper/vag2.html">
+          <img src="works_on_paper/130_vag_collage_2_th.jpg" alt="Vagina Collage 2">
+        </a>
+      </li>
+
+      <li>
+        <a href="works_on_paper/do_it_yourself.html">
+          <img src="works_on_paper/140_do_it_yourself_th.jpg" alt="Do It Yourself">
+        </a>
+      </li>
+
+      <li>
+        <a href="works_on_paper/heartstrings.html">
+          <img src="works_on_paper/141_heartstrings_th.jpg" alt="Heartstrings">
+        </a>
+      </li>
+        
+      <li>
+        <a href="works_on_paper/vag3.html">
+          <img src="works_on_paper/142_vagina_collage_3_th.jpg" alt="Vagina Collage 3">
+        </a>
+      </li>
+      
+      <li>
+        <a href="works_on_paper/vag4.html">
+          <img src="works_on_paper/143_vagina_collage_4_th.jpg" alt="Vagina Collage 4">
+        </a>
+      </li>
+
+    </ul>
+
+  </div>
+                  
+  <?php include 'footer.php';?>
+
 </body>
 </html>
-<a href="http://mailhide.recaptcha.net/d?k=01mMPbr11C6AYPUzw4KXTWpA==&amp;c=mkIjaYy6ZbTx-y8jgV2gKsrLeOyMEgHVMZ4ZkU0JHaU=" onClick="window.open('http://mailhide.recaptcha.net/d?k=01mMPbr11C6AYPUzw4KXTWpA==&amp;c=mkIjaYy6ZbTx-y8jgV2gKsrLeOyMEgHVMZ4ZkU0JHaU=', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">
