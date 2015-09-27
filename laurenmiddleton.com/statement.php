307ce6bd0ba1dc92ba7a46cc232828107f3a59bf
Lauren Middleton <git@laurenmiddleton.com>
2015-09-27 15:25:32 -0400

    fixed statement layout

--- laurenmiddleton.com/statement.php
+++ laurenmiddleton.com/statement.php
@@ -6,37 +6,22 @@
 </head>
 
 <body>
-<table width="700" align="center" cellpadding="4" bordercolor="#ECECEC" id="maincontent" vspace="50">
 
   <?php include 'header.php';?>
 
-  <tr>
-    <td colspan="3" align="center" valign="middle" bordercolor="#FFFFFF" class="caption">
-      <div align="center">
-        <table width="700" border="0" align="center" cellpadding="4">
-          <tr>
-            <td width="318" style="text-align: center">
-              <p class="caption">I am interested in life and death cycles as they pertain to the physical body, as well as human psychology and relationships. Learning to let go of people, of past loves, of places I once called home, and of beliefs about myself and others is a process that I feel deeply and consistently. Objects that mirror these patterns in the natural world, in organic matter, and in my own body serve as powerful metaphors to visually depict these processes. The act of painting is a meditation on these emotional and physical cycles of holding on and letting go, of inhaling and exhaling, and of birth and death, that give physical form and interpretation to these experiences so that they may be examined, better understood, and accepted.</p>
-              <p class="caption">I use predominately red pigment, symbolic of blood, life, and passion, to paint these organic forms. Currently I am focusing on ambiguous structures that may simultaneously appear as placentas, nebulas, flowers, cardiac and vessel tissue, shell interiors, vulvas, and fruit-like forms. The ambiguity of these forms' identities allows for a multiplicity of meaning and a unification of a literal, physical experience of life and death forces (such as the female reproductive cycle or the blooming and withering of a flower), and a metaphorical, intangible experience of life and death (such as learning to let go of relationships and ways of being that no longer serve). In both cases, the synthesizing idea behind these works comes from the Jungian psychologist Clarissa Pinkola Estés' writings on what she calls the "Life/Death/Life" cycle, with which one learns to trust that the loss or diminishing of one thing will always be followed by an incoming flux of new life.</p>
-              <p class="caption">My work ultimately serves to honor the Life/Death/Life force and the parts of ourselves we are all called on, again and again, to leave behind.</p>
-              <p class="caption"><em>Lauren Middleton, February 2012</em></p>
-            </td>
-          </tr>
-        </table>
-      </div>
-    </td>
-  </tr>
+  <div class="maincontent">
 
+    <p >I am interested in life and death cycles as they pertain to the physical body, as well as human psychology and relationships. Learning to let go of people, of past loves, of places I once called home, and of beliefs about myself and others is a process that I feel deeply and consistently. Objects that mirror these patterns in the natural world, in organic matter, and in my own body serve as powerful metaphors to visually depict these processes. The act of painting is a meditation on these emotional and physical cycles of holding on and letting go, of inhaling and exhaling, and of birth and death, that give physical form and interpretation to these experiences so that they may be examined, better understood, and accepted.</p>
+
+    <p>I use predominately red pigment, symbolic of blood, life, and passion, to paint these organic forms. Currently I am focusing on ambiguous structures that may simultaneously appear as placentas, nebulas, flowers, cardiac and vessel tissue, shell interiors, vulvas, and fruit-like forms. The ambiguity of these forms' identities allows for a multiplicity of meaning and a unification of a literal, physical experience of life and death forces (such as the female reproductive cycle or the blooming and withering of a flower), and a metaphorical, intangible experience of life and death (such as learning to let go of relationships and ways of being that no longer serve). In both cases, the synthesizing idea behind these works comes from the Jungian psychologist Clarissa Pinkola Estés' writings on what she calls the "Life/Death/Life" cycle, with which one learns to trust that the loss or diminishing of one thing will always be followed by an incoming flux of new life.</p>
+
+    <p>My work ultimately serves to honor the Life/Death/Life force and the parts of ourselves we are all called on, again and again, to leave behind.</p>
+
+    <p>Lauren Middleton, February 2012</p>
+
+  </div>
+           
   <?php include 'footer.php';?>
 
-</table>
-<br />
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "UA-3411571-1";
-urchinTracker();
-</script>
 </body>
 </html>
-<a href="http://mailhide.recaptcha.net/d?k=01mMPbr11C6AYPUzw4KXTWpA==&amp;c=mkIjaYy6ZbTx-y8jgV2gKsrLeOyMEgHVMZ4ZkU0JHaU=" onClick="window.open('http://mailhide.recaptcha.net/d?k=01mMPbr11C6AYPUzw4KXTWpA==&amp;c=mkIjaYy6ZbTx-y8jgV2gKsrLeOyMEgHVMZ4ZkU0JHaU=', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">
